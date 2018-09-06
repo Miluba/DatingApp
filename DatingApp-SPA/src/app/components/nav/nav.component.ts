4e5d19f5d65946e8d3c8904417d47f8e68c41652
Michael Bauer <miluba154@gmail.com>
2018-09-06 13:51:42 +0200

    End of Section 5

--- DatingApp-SPA/src/app/components/nav/nav.component.ts
+++ DatingApp-SPA/src/app/components/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
         console.log('success');
       },
       error => {
-        console.log('error');
+        console.log(error);
       }
     );
   }
